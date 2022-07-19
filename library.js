e5e5e97c8d4ec2e7016a541719a67c9edd14eee2
Rosendo Martinez <rosendomartinez270@gmail.com>
2022-07-19 15:40:09 -0500

    Implement table display mode feature

--- library.js
+++ library.js
@@ -3,14 +3,24 @@ const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const numberOfPagesInput = document.querySelector('#number-of-pages');
 const numberOfReadPagesInput = document.querySelector('#number-of-read-pages');
-const cardsDiv = document.querySelector('.cards')
+const cardsContainer = document.querySelector('.cards')
+const table = document.querySelector('.table');
+const rowsContainer = document.querySelector('.rows');
+const tableDisplayModeBtn = document.querySelector('#table-display-mode')
+
+tableDisplayModeBtn.onclick = (e) => {
+    isTableDisplayModeOn = !isTableDisplayModeOn;
+    toggleDisplay();
+    renderLibrary();
+}
 
 form.onsubmit = (e) => {
     e.preventDefault();
     createAndAddBookToLibrary(titleInput.value, authorInput.value, numberOfPagesInput.value, numberOfReadPagesInput.value);
-    cardsDiv.innerHTML = getLibraryHTML();
+    renderLibrary();
 }
 
+let isTableDisplayModeOn = false;
 let myLibrary = [];
 
 function Book(title, author, numberOfPages, numberOfReadPages) {
@@ -32,16 +42,45 @@ Book.prototype.getCardHTML = function () {
     `
 }
 
+Book.prototype.getRowHTML = function () {
+    const {title, author, numberOfPages, numberOfReadPages} = this;
+    return `
+        <tr>
+            <td>${title}</td>
+            <td>${author}</td>
+            <td>${numberOfPages}</td>
+            <td>${numberOfReadPages}</td>
+        </tr>
+    `
+}
+
 function getLibraryHTML() {
     let libraryHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
-        libraryHTML += myLibrary[i].getCardHTML();
+        if (isTableDisplayModeOn) {
+            libraryHTML += myLibrary[i].getRowHTML();
+        } else {
+            libraryHTML += myLibrary[i].getCardHTML();
+        }
     };
     return libraryHTML;
 }
 
 function createAndAddBookToLibrary(title, author, numberOfPages, numberOfReadPages) {
     myLibrary.push(new Book(title, author, numberOfPages, numberOfReadPages));
+}
+
+function renderLibrary() {
+    if (isTableDisplayModeOn) {
+        rowsContainer.innerHTML = getLibraryHTML();
+    } else {
+        cardsContainer.innerHTML = getLibraryHTML();
+    }
+}
+
+function toggleDisplay() {
+    table.classList.toggle('hidden');
+    cardsContainer.classList.toggle('hidden');
 }
 
 
